748abc8afb2f9c4672fbcae296891b464ca4dcd7
Stevens Reality <stevensreality@outlook.com>
2022-10-27 23:22:05 +0100

    first commit

---
+++ musicapp/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.utils.timezone import datetime
+
+# Create your models here. 
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(default="", max_length=255, blank=False, null=False)
+    last_name = models.CharField(default="", max_length=255, blank=False, null=False)
+    age = models.IntegerField(blank=False, null=False, default="")
+
+
+    class Meta:
+        ordering = ("first_name", "last_name", "age")
+
+    
+    def __str__(self):  
+        return self.first_name
+
+
+
+class Song(models.Model):
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE, default="", blank=False, null=False)
+    title = models.CharField(max_length=255, default="", blank=False, null=False)
+    date_released = models.DateField(default=datetime.now, blank=False, null=False)
+    likes = models.IntegerField(default=0, blank=False, null=False)
+    id_artiste = models.CharField(max_length=100, default="", blank=False, null=False, unique=True, editable=True, primary_key=True)
+
+
+    class Meta:
+        ordering = ("title", "artiste", "date_released", "likes", "id_artiste",)
+
+
+    def __str__(self):
+        return self.title
+
+
+
+class Lyric(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, default="", blank=False, null=False)
+    content = models.TextField(default="", blank=False, null=False)
+    id_song = models.CharField(max_length=100, default="", blank=False, null=False, unique=True, editable=True, primary_key=True)
+
+
+    class Meta:
+        ordering = ("song", "content", "id_song",)
+
+
+    def __str__(self):
+        return self.content
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+## Music Web Application ##
+
+A Project from the __I4GxZuri Backend Python programming course__ 
+Built in Django 4.1 using Python 3.10.4
+
+Download [Python](https://python.org/download "Python") on your machine and install Django using the command on your command line:
+`pip install django`
+
+Run the local server from command prompt using the command:
+`python manage.py runserver`
