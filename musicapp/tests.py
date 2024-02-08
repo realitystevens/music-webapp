f73b1cea53a2625e5c25a0209d1f03785d296496
Stevens Reality <stevensreality@outlook.com>
2024-02-08 22:55:37 +0100

    Created current test commands for models.py

--- musicapp/tests.py
+++ musicapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Artiste, Song, Lyric
 
-# Create your tests here.
+
+
+class ArtisteTest(TestCase):
+    def setUp(self):
+        self.artiste = Artiste.objects.create(first_name='Test First Name')
+
+    def test_model_name(self):
+        self.assertEqual(self.artiste.first_name, 'Test First Name')
+
